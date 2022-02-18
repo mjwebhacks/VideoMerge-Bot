77f3e1247b1c238c80cb3ae57c6550c793b9cfe9
mjwebhacks <98449007+mjwebhacks@users.noreply.github.com>
2022-02-18 12:04:49 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,21 +4,20 @@ import os
 
 
 class Config(object):
-    API_ID = os.environ.get("API_ID")
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    SESSION_NAME = os.environ.get("SESSION_NAME", "Video-Merge-Bot")
-    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL")
+    API_ID = "3796974"
+    API_HASH = "9511d0112631f9990337eb724d1a7d0d"
+    BOT_TOKEN = "5288446787:AAGPvKJeD5gPzJfZ_fqx0tvO3-ruaVXxBzI"
+    SESSION_NAME = "Video-Merge-Bot"
+    UPDATES_CHANNEL = "-1001771465683"
     LOG_CHANNEL = os.environ.get("LOG_CHANNEL")
-    DOWN_PATH = os.environ.get("DOWN_PATH", "./downloads")
-    TIME_GAP = int(os.environ.get("TIME_GAP", 5))
-    MAX_VIDEOS = int(os.environ.get("MAX_VIDEOS", 5))
-    STREAMTAPE_API_USERNAME = os.environ.get("STREAMTAPE_API_USERNAME")
-    STREAMTAPE_API_PASS = os.environ.get("STREAMTAPE_API_PASS")
-    MONGODB_URI = os.environ.get("MONGODB_URI")
+    DOWN_PATH = "./downloads"
+    TIME_GAP = 5
+    MAX_VIDEOS = 10
+    STREAMTAPE_API_USERNAME = "c66b279ae191202bc510"
+    STREAMTAPE_API_PASS = "golO1z3WjYc3O0"
+    MONGODB_URI = "mongodb+srv://mjfreeflix:Manya007@cluster0.kinyk.mongodb.net/uploads?retryWrites=true&w=majority"
     BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
-    BOT_OWNER = int(os.environ.get("BOT_OWNER", 1445283714))
-
+    BOT_OWNER = 1464063686
     START_TEXT = """
 Hi Unkil, I am Video Merge Bot!
 I can Merge Multiple Videos in One Video. Video Formats should be same.
